92f814f1439c210bc1c7d66142d785b94ff07455
R2D2 <carturolopezm@gmail.com>
2022-12-21 22:10:57 -0500

    added changin cart size when cart quantity is modified

--- src/components/header/CartHeader.jsx
+++ src/components/header/CartHeader.jsx
@@ -1,14 +1,20 @@
 import { useGeneralContext } from "@/context/useGeneralContext";
 import CartIcon from "@/components/icons/CartIcon";
 import CartDetailsHeader from "@/components/header/CartDetailsHeader";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import useClickOutside from "@/hooks/useClickOutside";
 
 const CartHeader = () => {
   const { cartQuantity } = useContext(useGeneralContext);
   const [isOpenCart, setIsOpenCart] = useState(false);
   const divRef = useClickOutside(() => setIsOpenCart(false));
-
+  const [newQ, setNewQ] = useState("text-xs");
+  useEffect(() => {
+    setNewQ("text-base");
+    setTimeout(() => {
+      setNewQ("text-xs");
+    }, 500);
+  }, [cartQuantity]);
   const handleOpenCart = () => {
     setIsOpenCart(!isOpenCart);
   };
@@ -23,7 +29,9 @@ const CartHeader = () => {
         className=" relative ml-auto flex min-h-[30px] min-w-[30px] justify-end self-center"
       >
         <CartIcon />
-        <span className=" absolute top-1 right-0 rounded-lg bg-green-primary px-1 text-xs font-bold text-white">
+        <span
+          className={`absolute top-1 right-0 rounded-lg bg-green-primary px-1 ${newQ} font-bold text-white`}
+        >
           {cartQuantity}
         </span>
       </button>
